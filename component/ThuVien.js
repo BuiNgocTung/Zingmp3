4c5782b0620cd89900176e2fa4804957ad5c0ac0
BuiNgocTung <buingoctung1990@gmail.com>
2023-11-06 00:54:42 +0700

    update thu vien

--- component/ThuVien.js
+++ component/ThuVien.js
@@ -1,17 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image, TextInput, TouchableOpacity, ScrollView ,FlatList} from 'react-native';
-import React, { useEffect,useState } from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome'
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ScrollView, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App({ navigation }) {
-  const dataMusic = [
+  const recentMusicData = [
     {
       id: 0,
       category: "ballad",
       name: "Gương Mặt Lạ Lẩm",
       image: require("../img/list/GuongMatLaLam.jpg"),
       time: '5:34',
-
     },
     {
       id: 1,
@@ -19,7 +18,6 @@ export default function App({ navigation }) {
       name: "Hết Thương cạn nhớ",
       image: require("../img/list/HetThuongCanNho.jpg"),
       time: '4:44',
-
     },
     {
       id: 2,
@@ -27,140 +25,142 @@ export default function App({ navigation }) {
       name: "Hết Thương cạn nhớ",
       image: require("../img/list/HetThuongCanNho.jpg"),
       time: '4:44',
-
     },
+  ];
 
-  ]
-  const [selectedButton, setSelectedButton] = useState('Playlist');
+  const [selectedTab, setSelectedTab] = useState('Playlist');
   const [showAdditionalButtons, setShowAdditionalButtons] = useState(true);
 
-  const handleButtonPress = (buttonName) => {
-    setSelectedButton(buttonName);
-    // Khi bạn chọn nút "Playlist," bạn hiển thị các nút bổ sung
-    if (buttonName === 'Playlist') {
+  const handleTabPress = (tabName) => {
+    setSelectedTab(tabName);
+    // Hiển thị nút bổ sung khi chọn tab "Playlist"
+    if (tabName === 'Playlist') {
       setShowAdditionalButtons(true);
     } else {
       setShowAdditionalButtons(false);
     }
   };
+
   return (
     <ScrollView>
-    <View style={styles.container}>
+      <View style={styles.container}>
 
-      <View style={styles.header}>
+        <View style={styles.header}>
+          {/* Thanh header */}
+          <TouchableOpacity onPress={() => { navigation.navigate("User") }}>
+            <Image source={require('../img/user/tung.jpg')} style={styles.userImage} />
+          </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => { navigation.navigate("User") }}>
-          <Image source={require('../img/user/tung.jpg')} style={{ width: 50, height: 50, borderRadius: 150, paddingLeft: 10 }} />
-        </TouchableOpacity>
-
-        <View style={{ flexDirection: 'row' }}>
-          <Icon name='search' style={styles.iconSearch} size={20} />
-
-          <TextInput placeholder='Tìm kiếm bài hát, nghệ sĩ...' style={styles.input} />
-          <Icon name='microphone' style={styles.iconSpeak} size={20} />
-
-        </View>
-        <TouchableOpacity onPress={() => { navigation.navigate("Setting") }}>
-          <Icon name='gear' size={20} />
-        </TouchableOpacity>
-
-
-
-      </View>
-      <Text style={{ fontSize: 25, fontWeight: '700', margin: 20 }}>Thư viện</Text>
-
-      <View style={{ flexDirection: 'row' }}>
-
-        <TouchableOpacity>
-          <View style={styles.upgrade}>
-
-            <Image source={require('../icon/library/favorite.png')} style={{ width: 48, height: 51 }} />
-            <Text style={styles.text}>Bài hát yêu thích</Text>
-
-            <Text style={[styles.text, { color: '#875454' }]}>77</Text>
-
+          <View style={styles.searchBar}>
+            <Icon name='search' style={styles.searchIcon} size={20} />
+            <TextInput placeholder='Tìm kiếm bài hát, nghệ sĩ...' style={styles.input} />
+            <Icon name='microphone' style={styles.microphoneIcon} size={20} />
           </View>
-        </TouchableOpacity>
 
-        <TouchableOpacity>
-          <View style={styles.upgrade}>
-            <Icon name='download' size={48} />
-            <Text style={styles.text}>Đã tải</Text>
-            <Text style={[styles.text, { color: '#875454' }]}>0</Text>
+          <TouchableOpacity onPress={() => { navigation.navigate("Setting") }}>
+            <Icon name='gear' size={20} />
+          </TouchableOpacity>
+        </View>
+
+        {/* Tiêu đề */}
+        <Text style={styles.title}>Thư viện</Text>
+
+        {/* Phần nút "Bài hát yêu thích" và "Đã tải" */}
+        <View style={styles.upgradeContainer}>
+          <TouchableOpacity>
+            <View style={styles.upgradeItem}>
+              <Image source={require('../icon/library/favorite.png')} style={styles.favoriteIcon} />
+              <Text style={styles.upgradeText}>Bài hát yêu thích</Text>
+              <Text style={styles.upgradeCount}>77</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View style={styles.upgradeItem}>
+              <Icon name='download' size={48} style={styles.downloadIcon} />
+              <Text style={styles.upgradeText}>Đã tải</Text>
+              <Text style={styles.upgradeCount}>0</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        {/* Danh sách nhạc nghe gần đây */}
+        <View style={styles.recentMusicContainer}>
+          <Text style={styles.recentMusicTitle}>Nghe gần đây</Text>
+          <FlatList
+            horizontal={true}
+            data={recentMusicData}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <TouchableOpacity style={styles.recentMusicItem}>
+                <Image source={item.image} style={styles.recentMusicImage} />
+                <Text>{item.name}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+
+        {/* Chuyển đổi tab "Playlist" và "Album" */}
+        <View style={styles.tabContainer}>
+          <TouchableOpacity
+            style={[styles.tabButton, selectedTab === 'Playlist' && styles.selectedTabButton]}
+            onPress={() => handleTabPress('Playlist')}>
+            <Text
+              style={[
+                styles.tabButtonText,
+                selectedTab === 'Playlist' && styles.selectedTabButtonText,
+              ]}
+            >
+              Playlist
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.tabButton, selectedTab === 'Album' && styles.selectedTabButton]}
+            onPress={() => handleTabPress('Album')}>
+            <Text
+              style={[
+                styles.tabButtonText,
+                selectedTab === 'Album' && styles.selectedTabButtonText,
+              ]}
+            >
+              Album
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Nút bổ sung khi chọn tab "Playlist" */}
+        {showAdditionalButtons==true && (
+          <View style={styles.additionalButtonsContainer}>
+            <TouchableOpacity style={styles.additionalButton}>
+              <View style={styles.buttonIconContainer}>
+                <Icon name='plus' size={20} style={styles.buttonIcon} />
+              </View>
+              <Text style={styles.additionalButtonText}>Tạo playlist</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.additionalButton}>
+              <View style={styles.buttonIconContainer}>
+                <Icon name='music' size={20} style={styles.buttonIcon} />
+              </View>
+              <Text style={styles.additionalButtonText}>Những bài nhạc hay của PMQ</Text>
+            </TouchableOpacity>
           </View>
-        </TouchableOpacity>
+        )}
 
+        {/* Thông báo khi chưa có playlist */}
+        {showAdditionalButtons==false && (
+          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{
+              justifyContent: 'center', alignItems: 'center',
+              borderWidth: 1, borderColor: 'gray', width: 40, height: 40, borderRadius: 10
+            }}>
+              <Icon name='music' size={20} /></View>
+            <Text style={{ fontSize: 16, fontWeight: '700', marginTop: 10 }}>Bạn chưa có album nào</Text>
+            <Text style={{ fontSize: 14, marginTop: 10 }}>Tìm album bạn yêu thích để thêm</Text>
+            <Text style={{ fontSize: 14, }}>vào thư viện</Text>
+          </View>
+        )}
       </View>
-
-      <View style={{margin:20}}>
-        <Text style={{ fontSize: 21, fontWeight: '700' }}>Nghe gần đây </Text>
-        {/* Nhạc nghe gần đây */}
-        <FlatList
-        horizontal ={true}
-        data={dataMusic}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-
-          <TouchableOpacity style={{alignItems:'center',justifyContent:'center', marginLeft:-10}}>
-          
-              <Image source={item.image} style={ styles.imgMusic} />
-         
-            <Text >{item.name}</Text>
-          </TouchableOpacity>
-
-        )} />
-      </View>
-            {/* Playlist / album */}
-           
-
-            <View style={{flexDirection:'row',margin:20}}>
-              <TouchableOpacity style={[styles.buttonPlaylist,selectedButton==='Playlist' && styles.selectedButton]}
-              onPress={()=> handleButtonPress('Playlist')}>
-                <Text style={{fontSize:18}}>Playlist</Text>
-              </TouchableOpacity>
-
-                <TouchableOpacity style={[styles.buttonPlaylist,selectedButton==='Album' && styles.selectedButton]}
-              onPress={()=> handleButtonPress('Album')}>
-                <Text style={{fontSize:18}}>Album</Text>
-              </TouchableOpacity>
-            </View>
-
-
-          {/* Nếu đã chọn "Playlist," hiển thị các nút bổ sung */}
-      {showAdditionalButtons==true && (
-        <View>
-          <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <View style={styles.buttonList}>
-              <Icon name="plus" size={20} />
-            </View>
-            <Text style={{ fontSize: 14 }}>Tạo playlist</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <View style={styles.buttonList}>
-              <Icon name="music" size={20} />
-            </View>
-            <Text style={{ fontSize: 14 }}>Những bài nhạc hay của PMQ</Text>
-          </TouchableOpacity>
-        </View>
-      )}
-      
-      {showAdditionalButtons==false && (
-        <View style={{justifyContent:'center',alignItems:'center'}}>
-              <View style={{justifyContent:'center',alignItems:'center',
-              borderWidth:1,borderColor:'gray',width:40,height:40,borderRadius:10}}> 
-              <Icon name='music' size={20}/></View>     
-              <Text style={{fontSize:16,fontWeight:'700',marginTop:10}}>Bạn chưa có album nào</Text>
-              <Text style={{fontSize:14,marginTop:10}}>Tìm album bạn yêu thích để thêm</Text>
-              <Text style={{fontSize:14,}}>vào thư viện</Text>
-        </View>
-      )}
-          
-
-         
-
-            
-    </View>
     </ScrollView>
   );
 }
@@ -168,21 +168,35 @@ export default function App({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
-    paddingTop: 10
-
-  },
-  scrollContainer: {
-
-    flexDirection: 'row',
-    marginTop: 10,
-    height: 220
+    paddingTop: 10,
   },
   header: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-evenly',
-
+    justifyContent: 'space-around',
+    marginLeft:10,
+    marginRight:10
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 150,
+    paddingLeft: 10,
+  },
+  searchBar: {
+    flexDirection: 'row',
+  },
+  searchIcon: {
+    position: 'absolute',
+    marginLeft: 10,
+    marginTop: 10,
+    color: '#8E86A8',
+  },
+  microphoneIcon: {
+    color: '#8E86A8',
+    position: 'absolute',
+    marginLeft: 210,
+    marginTop: 10,
   },
   input: {
     paddingLeft: 40,
@@ -192,26 +206,15 @@ const styles = StyleSheet.create({
     width: 245,
     height: 37,
   },
-  iconSearch: {
-
-    position: 'absolute',
-    marginLeft: 10,
-    marginTop: 10,
-    color: '#8E86A8',
-
-
-
+  title: {
+    fontSize: 25,
+    fontWeight: '700',
+    margin: 20,
   },
-
-  iconSpeak: {
-    color: '#8E86A8',
-    position: 'absolute',
-    marginLeft: 210,
-    marginTop: 10,
-
-
+  upgradeContainer: {
+    flexDirection: 'row',
   },
-  upgrade: {
+  upgradeItem: {
     margin: 10,
     width: 170,
     height: 150,
@@ -230,12 +233,36 @@ const styles = StyleSheet.create({
     flexShrink: 0,
     alignItems: 'center',
     justifyContent: 'space-around',
-
   },
-
-  imgMusic: {
+  favoriteIcon: {
+    width: 48,
+    height: 51,
+  },
+  upgradeText: {
+    fontWeight: '800',
+    fontSize: 12,
+  },
+  upgradeCount: {
+    color: '#875454',
+  },
+  downloadIcon: {
+    width: 48,
+    height: 51,
+  },
+  recentMusicContainer: {
+    margin: 20,
+  },
+  recentMusicTitle: {
+    fontSize: 21,
+    fontWeight: '700',
+  },
+  recentMusicItem: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: -10,
+  },
+  recentMusicImage: {
     margin: 10,
-   
     width: 160,
     height: 165,
     borderRadius: 30,
@@ -253,29 +280,74 @@ const styles = StyleSheet.create({
     flexShrink: 0,
     alignItems: 'center',
     justifyContent: 'space-around',
-
   },
-  text: {
-    fontWeight: '800',
-    fontSize: 12
+  tabContainer: {
+    flexDirection: 'row',
+    margin: 20,
   },
-  selectedButton: {
+  tabButton: {
+    borderBottomWidth: 1,
+    borderColor: 'transparent',
+    width: 70,
+    height: 30,
+    marginRight: 10,
+  },
+  selectedTabButton: {
     borderBottomColor: '#AB43AD',
     borderBottomWidth: 1,
   },
-  selectedText: {
-  
-    fontWeight: 'bold', // Làm cho chữ đậm khi nút được chọn
+  tabButtonText: {
+    fontSize: 18,
   },
-  buttonPlaylist:{
-    borderBottomWidth: 1,
-    borderColor: 'transparent', // Loại bỏ viền ban đầu
-    width:70,
-    height:30,
-    marginRight:10
+  selectedTabButtonText: {
+    fontWeight: 'bold',
   },
-  buttonList:{
-    backgroundColor:'#C0BBBB',width:50,height:40,borderRadius:10,
-            justifyContent:'center',alignItems:'center',margin:10
-  }
+  additionalButtonsContainer: {
+    flexDirection: 'column',
+  },
+  additionalButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  buttonIconContainer: {
+    backgroundColor: '#C0BBBB',
+    width: 50,
+    height: 40,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+  buttonIcon: {
+    color: 'black',
+  },
+  additionalButtonText: {
+    fontSize: 14,
+  },
+  noAlbumMessageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'gray',
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+  },
+  noAlbumIconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'gray',
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+  },
+  noAlbumIcon: {
+    fontSize: 20,
+  },
+  noAlbumText: {
+    fontSize: 16,
+    fontWeight: '700',
+    marginTop: 10,
+  },
 });
